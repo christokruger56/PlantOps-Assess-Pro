05b2584d05f09e2adf4a2fb71c2cf2bf7bc50d50
C <christo.kruger56@gmail.com>
2025-12-03 08:05:37 +0200

    Add files via upload

---
+++ csw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'equip-assess-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',  // Your Dashboard.html renamed to index.html
+  '/dump-truck.html',
+  '/road-grader.html',
+  '/bachoe.html',
+  '/excavator.html',
+  '/tracked-dozer.html',
+  '/roller.html',
+  '/front-end-loader.html',
+  // Add any CSS/JS if external, but yours are inline
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
